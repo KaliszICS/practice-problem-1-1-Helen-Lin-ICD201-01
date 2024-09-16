3882ec5149ddcb1b625f4acf23f69e94fc99899e
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-09-16 14:13:38 +0000

    a

--- PP1_1.py
+++ PP1_1.py
@@ -1,12 +1,12 @@
 def q1():
   #Write Assignment code here
-
+  print ("Hello World")
 def q2():
   #Write Assignment code here
   
 def q3():
   #Write Assignment code here
-  
+
 def q4:
   #Write Assignment code here
 
